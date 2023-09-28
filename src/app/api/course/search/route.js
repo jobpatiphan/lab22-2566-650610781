152e44afea7e7f931d7b1a1d8ae6e0f8091e34a5
Mr.Job <patiphan_klinhom@cmu.ac.th>
2023-09-28 15:35:53 +0700

    Completed!!!

--- src/app/api/course/search/route.js
+++ src/app/api/course/search/route.js
@@ -5,8 +5,16 @@ export const GET = async (request) => {
   const searchText = request.nextUrl.searchParams.get("searchText");
   const prisma = getPrisma();
 
-  //Modify following line so that it find course with "searchText" variable
-  const courses = await prisma.course.findMany({});
+  //Modify following line so that it find course with "searchText" variable substring in its title
+  const courses = await prisma.course.findMany({
+    where: {
+      title: {
+        contains: searchText,
+        mode: "insensitive",
+      },
+    },
+  });
+  console.log(courses);
 
   return NextResponse.json({ ok: true, courses });
 };
